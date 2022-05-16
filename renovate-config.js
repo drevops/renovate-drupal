1a1eeb68358da6680ead939ec3deeef2f5097e2c
Alex Skrypnyk <alex@integratedexperts.com>
2022-05-16 18:51:16 +1000

    Updated to not use a config file.

--- renovate-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dependencyDashboardTitle: 'RenovateBot Dependency Dashboard (self-hosted)',
-    platform: 'github',
-    gitAuthor: 'RenovateBot Self Hosted <renovatebot@drevops.com>',
-    repositories: [
-        "drevops/renovate-circleci-drupal-example"
-    ]
-};
