9530815480db032f0bde4f385605f64d2cbb3a1f
Alex Skrypnyk <alex@integratedexperts.com>
2022-05-16 11:37:59 +1000

    t1

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # renovate-circleci-drupal-example
-Example to run RenovateBot in CircleCI on Drupal project.
+Example to run Renovate bot on a Drupal project.
+
+## Features
+
+1. 2 schedules: 
+   1. Minor and patch versions for critical Drupal core and related packages to 
+      run daily raised in `deps/minor-patch-core` branch.
+   2. All versions for all other packages to run weekly raised in 
+      `deps/all-contrib` branch.
+2. Add a custom label `dependencies` on PR creation.
+3. Add assignees on PR creation.
+
+## Expected minor and patch updates for core
+
+```
+  - drupal/core-composer-scaffold (9.2.0 -> 9.3.13)
+  - drupal/core-project-message (9.3.12 -> 9.3.13)
+  - drupal/core-recommended	(9.3.12 -> 9.3.13)
+```
+
+## Expected major, minor and patch updates for contribs
+
+```
+  - drevops/renovate-example-source (1.0.0 => 1.0.4)
+  - drevops/renovate-example-source2 (1.0.0 => 1.2.0)
+  - drupal/coffee (1.0.0 => 1.2.0)
+  - drupal/core-dev	(9.3.12 -> 9.3.13)
+  - drupal/core-composer-scaffold (9.2.0 => 9.3.12)
+  - vlucas/phpdotenv (v5.0.0 => v5.4.1)
+  - drupal/core-composer-scaffold (9.2.0 => 9.3.12)
+  - drevops/renovate-example-source (1.0.0 => 1.0.4)
+  - drevops/renovate-example-source2 (1.0.0 => 1.2.0)
+  - drupal/coffee (1.0.0 => 1.2.0)
+  - vlucas/phpdotenv (v5.0.0 => v5.4.1)
+  - drush/drush (10.* => 11.*)
+  - drevops/renovate-example-source3 (1.* => 3.*)
+```
+
+---
+
+See https://github.com/drevops/renovate-example for more examples.
---
+++ renovate-config.js
@@ -0,0 +1,55 @@
+Object.assign(process.env, {
+    GIT_AUTHOR_NAME: 'RenovateBot Self Hosted',
+    GIT_AUTHOR_EMAIL: 'renovatebot@drevops.com',
+    GIT_COMMITTER_NAME: 'RenovateBot Self Hosted',
+    GIT_COMMITTER_EMAIL: 'renovatebot@drevops.com',
+});
+
+module.exports = {
+    platform: 'github',
+    "branchPrefix": "deps/",
+    "labels": ["dependencies", "Dependencies"],
+    "assignees": [
+        "AlexSkrypnyk"
+    ],
+    "ignorePresets": [":prHourlyLimit2"],
+    "rangeStrategy": "update-lockfile",
+    "packageRules": [
+        {
+            "groupName": "Minor and Patch Core",
+            "groupSlug": "minor-patch-core",
+            "schedule": ["before 2am"],
+            "matchDatasources": ["packagist"],
+            "matchUpdateTypes": ["patch", "minor"],
+            "matchPackageNames": [
+                "drupal/core-composer-scaffold",
+                "drupal/core-project-message",
+                "drupal/core-recommended"
+            ]
+        },
+        {
+            "groupName": "Major Core - skipped to update manually",
+            "matchDatasources": ["packagist"],
+            "matchUpdateTypes": ["major"],
+            "matchPackageNames": [
+                "drupal/core-composer-scaffold",
+                "drupal/core-project-message",
+                "drupal/core-recommended"
+            ],
+            "enabled": false
+        },
+        {
+            "groupName": "All Contrib",
+            "groupSlug": "all-contrib",
+            "schedule": ["every weekend"],
+            "matchDatasources": ["packagist"],
+            "separateMajorMinor": false,
+            "matchPackagePatterns": [".*"],
+            "excludePackageNames": [
+                "drupal/core-composer-scaffold",
+                "drupal/core-project-message",
+                "drupal/core-recommended"
+            ]
+        }
+    ]
+};
