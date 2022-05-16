2be236e1d28a501cbf185823e1c6eea871a555f3
Alex Skrypnyk <alex@integratedexperts.com>
2022-05-16 11:14:29 +1000

    Initial commit

---
+++ renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dependencyDashboardTitle: 'RenovateBot Dependency Dashboard (self-hosted)',
+    platform: 'github',
+    gitAuthor: 'RenovateBot Self Hosted <renovatebot@drevops.com>',
+    repositories: [
+        "drevops/renovate-circleci-drupal-example"
+    ]
+};
